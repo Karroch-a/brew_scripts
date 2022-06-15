8e3032a62c4a7d6339574376d2b3e9f078412e08
Abdellah Azeroual <aazeroua@e3r3p11.1337.ma>
2022-06-15 20:05:47 +0100

    update

--- brew.sh
+++ brew.sh
@@ -1,4 +1,4 @@
-git clone --branch 2.0.6 https://github.com/Homebrew/brew.git $HOME/brew
-export PATH=$HOME/brew/bin:$PATH
-echo 'export PATH=$HOME/brew/bin:$PATH' >> $HOME/.zshrc
+git clone --branch 2.0.6 https://github.com/Homebrew/brew.git $HOME/goinfre/brew
+export PATH=$HOME/goinfre/brew/bin:$PATH
+echo 'export PATH=$HOME/goinfre/brew/bin:$PATH' >> $HOME/.zshrc
 source ~/.zshrc
